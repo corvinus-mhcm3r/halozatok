f9792008c83e746841f3fd6692584eaaeb6162d4
Norbert Salavecz <norbert.salavecz@stud.uni-corvinus.hu>
2021-03-21 23:57:23 +0100

    Initial commit

---
+++ Hajósteszt/Hajósteszt/wwwroot/jsgyak.js
@@ -0,0 +1,17 @@
+﻿window.onload = function()
+{
+    console.log("start")
+    let hova = document.getElementById("pascal");
+    for (var s = 0; s < 10; s++) {
+        let sor = document.createElement("div");
+        sor.classList.add("sor")
+        hova.appendChild(sor);
+
+        for (var o = 0; o < s; o++) {
+            let szám = document.createElement("div");
+            szám.innerText = (s + 1) * (o + 1)
+            sor.appendChild(szám);
+        }
+    }
+}
+
